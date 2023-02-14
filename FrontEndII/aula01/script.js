3a5e5c166be1dad03b91471830ca7659cf22e0d7
daninafromer <32798611+daninafromer@users.noreply.github.com>
2023-02-13 19:58:01 -0500

    Add files via upload

---
+++ FrontEndII/aula01/script.js
@@ -0,0 +1,6 @@
+// Declarando uma variável
+var name = window.prompt('Olá, estanho! Qual o seu nome?')
+let age = window.prompt('Qual a sua idade?')
+let job = window.prompt('Qual a sua profissão?')
+
+console.log("Olá me chamo " + name + ", tenho " + age + " e atualmente trabalho com " + job)
