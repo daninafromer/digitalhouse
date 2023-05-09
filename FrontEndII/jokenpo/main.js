14e2062d03cbc248889c5a6b2662fac1ff6b77fc
daninafromer <32798611+daninafromer@users.noreply.github.com>
2023-05-09 19:24:16 -0400

    Update main.js

--- FrontEndII/jokenpo/main.js
+++ FrontEndII/jokenpo/main.js
@@ -1,88 +1,61 @@
-var usuarioDesejaJogar = confirm("Você quer jogar Pedra, Papel ou Tesoura?")
-let vitorias = 0
-let derrotas = 0
+  // Armazenar a pontuação atual e o vencedor da rodada anterior
+  var pontuacao = 0;
+  var ultimoVencedor = null;
+  
+  var jogarNovamente = true;
 
-function segueOJogo(){
-    let jogadaUsuario = prompt("Escolha um número: 1-papel, 2-tesoura, 3-pedra")
-    let jogadaOponente = Math.floor(Math.random() * 3) + 1
+while (jogarNovamente) {
+  // Definir as opções do jogo
+  var jogadaUsuario = prompt("Você quer jogar Pedra, Papel ou Tesoura? Para Pedra digite 1, para papel digite 2, para tesoura digite 3:");
+  var jogadaUsuarioConvertida = Number(jogadaUsuario);
 
-    console.log("Você escolheu " + jogadaUsuario)
-    console.log("Seu oponente escolheu " + jogadaOponente)
+  // Selecionar uma opção aleatória para o computador
+  var jogadaScript = Math.floor(Math.random() * 3) + 1;
+
+  // Determinar quem ganhou a rodada
+  function playRound(jogadaUsuarioConvertida, jogadaScript) {
+    if (jogadaUsuarioConvertida === jogadaScript) {
+      return " Deu empate!";
+    } else if (
+      (jogadaUsuarioConvertida === 1 && jogadaScript === 3) ||
+      (jogadaUsuarioConvertida === 2 && jogadaScript === 1) ||
+      (jogadaUsuarioConvertida === 3 && jogadaScript === 2)
+    ) {
+      // Se o jogador ganhar duas vezes seguidas, atualizar a pontuação
+      if (ultimoVencedor === "jogador") {
+        pontuacao++;
+        return "Você ganhou novamente! Parabéns! Você se tornou campeã(o)! Sua pontuação é " + pontuacao + ".";
+      } else {
+        ultimoVencedor = "jogador";
+        return "Você ganhou essa batalha, mas para ser campeã(o), precisa ganhar duas vezes!";
+      }
+    } else {
+      // Se o computador ganhar duas vezes seguidas, reiniciar a pontuação
+      if (ultimoVencedor === "computador") {
+        pontuacao = 0;
+        ultimoVencedor = null;
+        return "Você perdeu novamente! Sua pontuação foi reiniciada.";
+      } else {
+        ultimoVencedor = "computador";
+        return "Você perdeu!";
+      }
+    }
+  }
+
+  // Executar uma rodada do jogo
+  const result = playRound(jogadaUsuarioConvertida, jogadaScript);
+
+  // Exibir o resultado
+  alert("Você jogou " + jogadaUsuarioConvertida + " e o computador jogou " + jogadaScript + ". " + result);
+
+  // Perguntar ao usuário se ele deseja jogar novamente
+  var resposta = prompt("Deseja jogar novamente? Digite 'sim' para jogar novamente ou qualquer outra coisa para sair:");
+
+  // Verificar a resposta do usuário
+  if (resposta.toLowerCase() !== "sim") {
+    jogarNovamente = false;
+  }
 }
 
-function pontuacao(){
-    let jogadaUsuario = prompt("Escolha um número: 1-papel, 2-tesoura, 3-pedra")
-    let jogadaOponente = Math.floor(Math.random() * 3) + 1
-
-    console.log("Você escolheu " + jogadaUsuario)
-    console.log("Seu oponente escolheu " + jogadaOponente)
-
-    if(jogadaOponente == jogadaUsuario){
-        console.log("Deu empate.")
-        console.log("Vitórias: " + vitorias)
-        console.log("Derrotas: " + derrotas)
-        pontuacao()
-    }
-    else if(jogadaOponente === 1 && jogadaUsuario === 3){
-        console.log("O papel ganha da pedra embrulhando-a.")
-        derrotas = derrotas + 1
-        console.log("Vitórias: " + vitorias)
-        console.log("Derrotas: " + derrotas)
-        if(derrotas === 2){
-            console.log("Você perdeu duas vezes. O jogo acabou.")
-            console.log(usuarioDesejaJogar)
-        }
-        else{
-            pontuacao()
-        }
-    }
-    else if(jogadaOponente === 3 && jogadaUsuario ===1){
-        console.log("O papel ganha da pedra embrulhando-a.")
-        vitorias = vitorias + 1
-        console.log("Vitórias: " + vitorias)
-        console.log("Derrotas: " + derrotas)
-        if(vitorias === 2){
-            console.log("Você venceu. Parabéns!")
-            console.log(usuarioDesejaJogar)
-        }
-        else{
-            pontuacao()
-        }
-    }
-    else if(jogadaOponente < jogadaUsuario){
-        console.log("Você ganhou!")
-        vitorias = vitorias + 1
-        console.log("Vitórias: " + vitorias)
-        console.log("Derrotas: " + derrotas)
-        if(vitorias === 2){
-            console.log("Você venceu. Parabéns!")
-            console.log(usuarioDesejaJogar)
-        }
-        else{
-            pontuacao()
-        }
-    }
-    else if(jogadaOponente > jogadaUsuario){
-        console.log("Você perdeu!")
-        derrotas = derrotas + 1
-        console.log("Vitórias: " + vitorias)
-        console.log("Derrotas: " + derrotas)
-        if(derrotas === 2){
-            console.log("Você perdeu duas vezes. O jogo acabou.")
-            console.log(usuarioDesejaJogar)
-        }
-        else{
-            pontuacao()
-        }
-    }
-}
-
-if(usuarioDesejaJogar === true){
-
-    pontuacao()
-
-}else {
-    alert("Então pegue descendo.")
-
-}
-
+// Exibir mensagem de encerramento
+alert("Obrigado por jogar! Até a próxima."); 
